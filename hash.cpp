815bd74d2fb5c17923c52bc3761ccb2df053c33b
Breno Leitao <brenohl@br.ibm.com>
2015-10-07 11:19:14 -0400

    New changes to output things properly

--- hash.cpp
+++ hash.cpp
@@ -9,14 +9,15 @@ const strhash_t STRHASH_MSB  = 0x80000000;
 strhash_t hash_string_i_fallback(const char *arKey, uint32_t nKeyLength) {
   uint64_t h[2];
   MurmurHash3::hash128<false>(arKey, nKeyLength, 0, h);
+  std::cout << "Output: " << std::hex << h[0] << std::endl;
   return strhash_t(h[0] & STRHASH_MASK);
 }
 
 
 int main(int argc, char **argv){
 	strhash_t foo;
-	hash_string_i_fallback("foobar", 4);
-	std::cout << foo << std::endl;
+	foo = hash_string_i_fallback("foobar", 4);
+	std::cout << "int32_t value: " << std::hex << foo << std::endl;
 
 	return 0;
 }
