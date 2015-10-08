fae36697566d8e9ed664a42ea6817f773728a193
Breno Leitao <brenohl@br.ibm.com>
2015-10-08 15:01:15 -0400

    Running both optimized and not-optimized versions

--- hash.cpp
+++ hash.cpp
@@ -10,22 +10,30 @@ extern "C" {
   strhash_t hash_string_i_unaligned_crc(const char*, uint32_t);
 }
 
-strhash_t hash_string_i_fallback(const char *arKey, uint32_t nKeyLength) {
+strhash_t hash_string_i_fallback(bool optimized, const char *arKey, uint32_t nKeyLength) {
   uint64_t h[2];
+  if (optimized){
 #ifdef __powerpc64__
-  h[0] = hash_string_i_unaligned_crc(arKey, nKeyLength);
+	  h[0] = hash_string_i_unaligned_crc(arKey, nKeyLength);
 #else
-  MurmurHash3::hash128<false>(arKey, nKeyLength, 0, h);
+	assert("Not running on a ppc64 machine\n");
 #endif
+  } else {
+	  MurmurHash3::hash128<false>(arKey, nKeyLength, 0, h);
+  }
+#ifdef DEBUG
   std::cout << "Output: " << std::hex << h[0] << std::endl;
+#endif 
   return strhash_t(h[0] & STRHASH_MASK);
 }
 
 
 int main(int argc, char **argv){
 	strhash_t foo;
-	foo = hash_string_i_fallback("foobar", 4);
-	std::cout << "int32_t value: " << std::hex << foo << std::endl;
+	foo = hash_string_i_fallback(true, "foobar", 4);
+	std::cout << "Optimized output value: " << std::hex << foo << std::endl;
+	foo = hash_string_i_fallback(false, "foobar", 4);
+	std::cout << "Unoptimzed output value: " << std::hex << foo << std::endl;
 
 	return 0;
 }
